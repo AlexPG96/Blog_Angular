3136f9795d8be08ceb65b5863fa9a5af77205b1d
Alejandro <ap96.gonzalez@gmail.com>
2020-05-17 10:59:07 +0200

    Modelo Post creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponentComponent } from './blog-component/blog-component.component';
+import { FormularioComponentComponent } from './formulario-component/formulario-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponentComponent,
+    FormularioComponentComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
